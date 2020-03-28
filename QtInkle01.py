4379bae2582899dc8cff8ae021e716844dc4e318
g0ukb <g0ukb@ivarc.org.uk>
2020-03-28 17:39:13 +0000

    First basic program

---
+++ QtInkle01.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = 'brian'
+
+from PySide2.QtWidgets import QApplication, QWidget
+from PySide2.QtGui import QIcon
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.setWindowTitle("QTInkle01")
+        self.setGeometry(300,300,300,300)
+        self.setMinimumHeight(100)
+        self.setMinimumWidth(250)
+        self.setMaximumHeight(300)
+        self.setMaximumWidth((400))
+        self.setIcon() # todo sort out whether is works on Mac and not on Cinnamon
+
+    def setIcon(self):
+        appIcon = QIcon("/home/brian/.local/share/icons/hicolor/16x16/apps/97C1_wordpad.0.png")
+        self.setWindowIcon(appIcon)
+
+
+myApp = QApplication(sys.argv)
+window = Window()
+window.show()
+
+myApp.exec_()
+sys.exit(0)
